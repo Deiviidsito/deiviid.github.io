3bdb5cd4c2fb1d7dbe05d3d6ea1a34f7380ffec1
Deiviidsito <david.alvarez.barraza@gmail.com>
2024-06-07 12:10:36 -0400

    fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,8 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
 import tailwind from "@astrojs/tailwind";
 
-// https://astro.build/config
 export default defineConfig({
-  site: 'https://deiviid.com',
-  integrations: [tailwind()]
+  site: "https://deiviid.com",
+  integrations: [tailwind()],
 });
